cb17c1378928c36980b8dab44f43b5967c056215
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-02 20:25:46 +0500

    Bank

---
+++ src/test/java/BankApp/SpringBank/SpringBankApplicationTests.java
@@ -0,0 +1,13 @@
+package BankApp.SpringBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
