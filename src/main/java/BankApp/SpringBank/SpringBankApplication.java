5597f2f5f168ee5b88faf80ab6c526ba5027c30c
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-03 00:16:18 +0500

    updated class

--- src/main/java/BankApp/SpringBank/SpringBankApplication.java
+++ src/main/java/BankApp/SpringBank/SpringBankApplication.java
@@ -2,8 +2,10 @@ package BankApp.SpringBank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class SpringBankApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class SpringBankApplication {
 	}
 
 }
+
