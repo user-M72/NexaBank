e456e7f3df492a01b9c4a80e9f43bcebed1384e0
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-03 18:04:19 +0500

    updated model class

--- src/main/java/BankApp/SpringBank/model/Account.java
+++ src/main/java/BankApp/SpringBank/model/Account.java
@@ -1,13 +1,26 @@
 package BankApp.SpringBank.model;
 
+import BankApp.SpringBank.model.Enum.AccountType;
+import BankApp.SpringBank.model.Enum.Currency;
+import BankApp.SpringBank.model.baseDomain.BaseDomain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 @Entity
 @Table(name = "accounts")
 @Getter
 @Setter
-public class Account {
+public class Account extends BaseDomain<UUID> {
+
+    private String accountNumber;
+    private BigDecimal balance = BigDecimal.ZERO;
+    private AccountType type;
+    private Currency currency;
+    private boolean isBlocked = false;
+
 }
--- src/main/java/BankApp/SpringBank/model/Transaction.java
+++ src/main/java/BankApp/SpringBank/model/Transaction.java
@@ -1,13 +1,26 @@
 package BankApp.SpringBank.model;
 
+import BankApp.SpringBank.model.Enum.TransactionStatus;
+import BankApp.SpringBank.model.Enum.TransactionType;
+import BankApp.SpringBank.model.baseDomain.BaseDomain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 @Entity
 @Table(name = "transactions")
 @Getter
 @Setter
-public class Transaction {
+public class Transaction extends BaseDomain<UUID> {
+
+    private BigDecimal amount;
+    private TransactionType type;
+    private TransactionStatus status;
+    private String description;
+    private String referenceNumber;
+
 }
