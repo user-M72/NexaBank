fa6d7bf02437191e92452794a29ca450b0f18b1e
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-06 03:52:52 +0500

    updated classes: toEntity and added updateFromDto

--- src/main/java/BankApp/SpringBank/mapper/AccountMapper.java
+++ src/main/java/BankApp/SpringBank/mapper/AccountMapper.java
@@ -3,7 +3,9 @@ package BankApp.SpringBank.mapper;
 import BankApp.SpringBank.dto.req.account.AccountRequestDto;
 import BankApp.SpringBank.dto.res.account.AccountResponseDto;
 import BankApp.SpringBank.model.Account;
+import BankApp.SpringBank.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -11,5 +13,7 @@ public interface AccountMapper {
 
     AccountResponseDto toDto(Account account);
 
-    Account toEntity(AccountRequestDto dto);
+    Account toEntity(AccountRequestDto dto, User user, String accountNumber);
+
+    void updateFromDto(AccountRequestDto dto, @MappingTarget Account account);
 }
