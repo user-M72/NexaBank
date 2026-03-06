bd66dcb12b8677fcd8323901fc1bdeea1a67d41d
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-06 16:59:13 +0500

    updated repository TransactionRepository

--- src/main/java/BankApp/SpringBank/repository/TransactionRepository.java
+++ src/main/java/BankApp/SpringBank/repository/TransactionRepository.java
@@ -1,9 +1,12 @@
 package BankApp.SpringBank.repository;
 
+import BankApp.SpringBank.model.Account;
 import BankApp.SpringBank.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+    List<Transaction> findByFromAccountOrToAccount(Account fromAccount, Account toAccount);
 }
