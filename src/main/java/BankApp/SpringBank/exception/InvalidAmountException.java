60b430dd00a7c72c8d89beb2e0fa813e40dfa102
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-06 23:44:34 +0500

    Added new exception class

---
+++ src/main/java/BankApp/SpringBank/exception/InvalidAmountException.java
@@ -0,0 +1,7 @@
+package BankApp.SpringBank.exception;
+
+public class InvalidAmountException extends RuntimeException {
+    public InvalidAmountException(String message) {
+        super(message);
+    }
+}
