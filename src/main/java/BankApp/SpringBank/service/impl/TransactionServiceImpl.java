39b72c0b669630d90d061a060ff71dd92df7a959
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-06 16:58:52 +0500

    updated and created new method for TransactionServiceImpl

--- src/main/java/BankApp/SpringBank/service/impl/TransactionServiceImpl.java
+++ src/main/java/BankApp/SpringBank/service/impl/TransactionServiceImpl.java
@@ -2,12 +2,20 @@ package BankApp.SpringBank.service.impl;
 
 import BankApp.SpringBank.dto.req.transaction.TransactionRequestDto;
 import BankApp.SpringBank.dto.res.transaction.TransactionResponseDto;
+import BankApp.SpringBank.exception.AccountBlockedException;
+import BankApp.SpringBank.exception.InsufficientFundsException;
+import BankApp.SpringBank.exception.TransactionNotFoundException;
 import BankApp.SpringBank.mapper.TransactionMapper;
+import BankApp.SpringBank.model.Account;
+import BankApp.SpringBank.model.Enum.TransactionStatus;
+import BankApp.SpringBank.model.Enum.TransactionType;
+import BankApp.SpringBank.model.Transaction;
 import BankApp.SpringBank.repository.TransactionRepository;
 import BankApp.SpringBank.service.AccountService;
 import BankApp.SpringBank.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -31,22 +39,84 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public TransactionResponseDto getById(UUID id) {
-        return null;
+        Transaction transaction = transactionRepository.findById(id)
+                .orElseThrow(()-> new TransactionNotFoundException(id));
+        return mapper.toDto(transaction);
     }
 
     @Override
+    @Transactional
     public TransactionResponseDto created(TransactionRequestDto dto) {
-        return null;
+        Account fromAccount = accountService.findById(dto.fromAccountId());
+        Account toAccount = accountService.findById(dto.toAccountId());
+
+        checkAccountNotBlocked(fromAccount);
+        checkSufficientFunds(fromAccount, dto.amount());
+
+        fromAccount.setBalance(fromAccount.getBalance().subtract(dto.amount()));
+        toAccount.setBalance(toAccount.getBalance().add(dto.amount()));
+
+        String referenceNumber = UUID.randomUUID().toString();
+
+        Transaction transaction = mapper.toEntity(dto, fromAccount, toAccount, referenceNumber);
+        transaction.setStatus(TransactionStatus.SUCCESS);
+
+        Transaction saved = transactionRepository.save(transaction);
+        return mapper.toDto(saved);
     }
 
     @Override
     public List<TransactionResponseDto> getByAccountId(UUID accountId) {
-        return List.of();
+        Account account = accountService.findById(accountId);
+
+        List<Transaction> transactions = transactionRepository.findByFromAccountOrToAccount(account, account);
+
+        return transactions.stream()
+                .map(mapper::toDto)
+                .toList();
     }
 
     @Override
+    @Transactional
     public TransactionResponseDto transfer(UUID fromId, UUID toId, BigDecimal amount) {
-        return null;
+        Account fromAccount = accountService.findById(fromId);
+        Account toAccount = accountService.findById(toId);
+
+        checkAccountNotBlocked(fromAccount);
+        checkSufficientFunds(fromAccount, amount);
+
+        fromAccount.setBalance(fromAccount.getBalance().subtract(amount));
+        toAccount.setBalance(toAccount.getBalance().add(amount));
+
+        Transaction transaction = Transaction.builder()
+                .amount(amount)
+                .type(TransactionType.TRANSFER)
+                .status(TransactionStatus.SUCCESS)
+                .fromAccount(fromAccount)
+                .toAccount(toAccount)
+                .description("Transfer from " + fromId + "to" + toId)
+                .referenceNumber(UUID.randomUUID().toString())
+                .build();
+
+        Transaction saved = transactionRepository.save(transaction);
+        return mapper.toDto(saved);
+    }
+
+    private Transaction findTransactionById(UUID id){
+        return transactionRepository.findById(id)
+                .orElseThrow(()-> new TransactionNotFoundException(id));
+    }
+
+    private void checkAccountNotBlocked(Account account){
+        if (account.isBlocked()){
+            throw new AccountBlockedException(account.getId());
+        }
+    }
+
+    private void checkSufficientFunds(Account account, BigDecimal amount){
+        if (account.getBalance().compareTo(amount) < 0){
+            throw new InsufficientFundsException(account.getBalance(), amount);
+        }
     }
 
 }
