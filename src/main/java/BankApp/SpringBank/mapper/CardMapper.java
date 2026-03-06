0cfd5afe77cb1eadb2a7ff93253e49238f309d3e
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-06 15:45:55 +0500

    updated CardMapper

--- src/main/java/BankApp/SpringBank/mapper/CardMapper.java
+++ src/main/java/BankApp/SpringBank/mapper/CardMapper.java
@@ -2,8 +2,10 @@ package BankApp.SpringBank.mapper;
 
 import BankApp.SpringBank.dto.req.card.CardRequestDto;
 import BankApp.SpringBank.dto.res.card.CardResponseDto;
+import BankApp.SpringBank.model.Account;
 import BankApp.SpringBank.model.Card;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -11,5 +13,7 @@ public interface CardMapper {
 
     CardResponseDto toDto(Card card);
 
-    Card toEntity(CardRequestDto dto);
+    Card toEntity(CardRequestDto dto, Account account, String cardNumber, String cvvHash);
+
+    void updateFromDto(CardRequestDto dto, @MappingTarget Card card);
 }
