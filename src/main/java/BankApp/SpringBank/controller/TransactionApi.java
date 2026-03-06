3c2601c567e06cd683199b0d5fa6f3e77b504789
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-06 16:57:51 +0500

    created new method for TransactionApi

--- src/main/java/BankApp/SpringBank/controller/TransactionApi.java
+++ src/main/java/BankApp/SpringBank/controller/TransactionApi.java
@@ -1,13 +1,16 @@
 package BankApp.SpringBank.controller;
 
+import BankApp.SpringBank.dto.req.transaction.TransactionRequestDto;
+import BankApp.SpringBank.dto.req.transfer.TransferRequestDto;
 import BankApp.SpringBank.dto.res.transaction.TransactionResponseDto;
 import BankApp.SpringBank.service.TransactionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +22,28 @@ public class TransactionApi {
     @GetMapping
     public List<TransactionResponseDto> get(){
         return service.get();
+    }
+
+    @GetMapping("/{transactionId}")
+    public TransactionResponseDto getById(@PathVariable("transactionId") UUID id){
+        return service.getById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<TransactionResponseDto> created(@RequestBody TransactionRequestDto dto){
+        TransactionResponseDto created = service.created(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @GetMapping("/account/{accountId}")
+    public ResponseEntity<List<TransactionResponseDto>> getByAccountId(@PathVariable("accountId") UUID id){
+        List<TransactionResponseDto> accountId = service.getByAccountId(id);
+        return ResponseEntity.ok(accountId);
+    }
+
+    @PostMapping("/transfer")
+    public ResponseEntity<TransactionResponseDto> transfer(@RequestBody TransferRequestDto dto){
+        TransactionResponseDto transfer = service.transfer(dto.fromAccount(), dto.toAccount(), dto.amount());
+        return ResponseEntity.status(HttpStatus.CREATED).body(transfer);
     }
 }
