ddfc435c907f3b1cb68b903e817eb609a71b31a5
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-06 16:57:25 +0500

    updated TransactionMapper

--- src/main/java/BankApp/SpringBank/mapper/TransactionMapper.java
+++ src/main/java/BankApp/SpringBank/mapper/TransactionMapper.java
@@ -2,14 +2,22 @@ package BankApp.SpringBank.mapper;
 
 import BankApp.SpringBank.dto.req.transaction.TransactionRequestDto;
 import BankApp.SpringBank.dto.res.transaction.TransactionResponseDto;
+import BankApp.SpringBank.model.Account;
 import BankApp.SpringBank.model.Transaction;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TransactionMapper {
 
+    @Mapping(target = "fromAccountId", source = "fromAccount.id")
+    @Mapping(target = "toAccountId", source = "toAccount.id")
     TransactionResponseDto toDto(Transaction transaction);
 
-    Transaction toEntity(TransactionRequestDto dto);
+    @Mapping(target = "status", constant = "PENDING")
+    @Mapping(target = "type", source = "dto.type")
+    @Mapping(target = "amount", source = "dto.amount")
+    @Mapping(target = "description", source = "dto.description")
+    Transaction toEntity(TransactionRequestDto dto, Account fromAccount, Account toAccount, String referenceNumber);
 }
