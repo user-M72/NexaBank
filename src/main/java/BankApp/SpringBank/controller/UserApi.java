2e9cc19256f8b5b37477fab4e0bd74d10173349b
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-08 03:06:57 +0500

    Added validation annotations to UserApi methods

--- src/main/java/BankApp/SpringBank/controller/UserApi.java
+++ src/main/java/BankApp/SpringBank/controller/UserApi.java
@@ -3,6 +3,7 @@ package BankApp.SpringBank.controller;
 import BankApp.SpringBank.dto.req.user.UserRequestDto;
 import BankApp.SpringBank.dto.res.user.UserResponseDto;
 import BankApp.SpringBank.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,13 +30,13 @@ public class UserApi {
     }
 
     @PostMapping
-    public ResponseEntity<UserResponseDto> created(@RequestBody UserRequestDto dto){
+    public ResponseEntity<UserResponseDto> created(@Valid @RequestBody UserRequestDto dto){
         UserResponseDto created = service.created(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
     @PutMapping("/{userId}")
-    public UserResponseDto updated(@PathVariable("userId") UUID id,
+    public UserResponseDto updated(@Valid @PathVariable("userId") UUID id,
                                    @RequestBody UserRequestDto dto){
         return service.updated(id, dto);
     }
