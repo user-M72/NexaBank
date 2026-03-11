2e9308f2caa5d1ee193b2e305bbe72bf8e63c5a8
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-12 01:05:50 +0500

    Implement custom user details and JWT authentication filter

---
+++ src/main/java/BankApp/SpringBank/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package BankApp.SpringBank.service;
+
+import BankApp.SpringBank.config.CustomUserDetails;
+import BankApp.SpringBank.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(()->new UsernameNotFoundException("User not found: " + username));
+    }
+}
