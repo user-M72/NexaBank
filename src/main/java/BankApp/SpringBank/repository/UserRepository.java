ed2ad189da039eb19c71827b2fa44b510dc1a0eb
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-12 23:59:45 +0500

    Update RoleApi and UserApi request mappings for consistency

--- src/main/java/BankApp/SpringBank/repository/UserRepository.java
+++ src/main/java/BankApp/SpringBank/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package BankApp.SpringBank.repository;
 
 import BankApp.SpringBank.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
 
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
